f8161b9bdd4013c708b52666f7a09ec386982b7d
muthukumaran23 <tvm.muthukumaran007@gmail.com>
2024-07-27 20:26:53 +0530

    update

--- routes/propertyRoute.js
+++ routes/propertyRoute.js
@@ -1,13 +1,20 @@
-const express= require("express")
-const router = express.Router()
-const {createProperty,getAllProperty,findSingleProperty,editProperty,deleteProperty} = require("../Controller/propertyController")
-const auth = require("../middleware/authMiddleware")
+const express = require("express");
+const router = express.Router();
+const {
+  createProperty,
+  getAllProperty,
+  SearchProperty,
+  findSingleProperty,
+  editProperty,
+  deleteProperty,
+} = require("../Controller/propertyController");
+const auth = require("../middleware/authMiddleware");
 
-
-router.post("/create",auth,createProperty)
-router.get("/getAll",auth,getAllProperty)
-router.get("/:id",auth,findSingleProperty)
-router.put("/:id",auth,editProperty)
-router.delete("/:id",auth,deleteProperty)
+router.post("/create", auth, createProperty);
+router.get("/getAll", auth, getAllProperty);
+router.get("/:keyword", auth, SearchProperty);
+router.get("/findOne/:id", auth, findSingleProperty);
+router.put("/:id", auth, editProperty);
+router.delete("/:id", auth, deleteProperty);
 
 module.exports = router;
